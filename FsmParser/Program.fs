7482a7ba46b5ee01176c1a43d272f9215b2eee66
Louis Lecailliez <louis.lecailliez@outlook.fr>
2015-08-01 03:16:03 +0200

    Lexer pour le fichier de graph fonctionnel.

--- FsmParser/Program.fs
+++ FsmParser/Program.fs
@@ -7,6 +7,7 @@ let main argv =
     0 // return an integer exit code
 *)
 
+(*
 open System
 open TestLexer
 open Microsoft.FSharp.Text
@@ -14,9 +15,26 @@ open Microsoft.FSharp.Text.Lexing
   
 let x = "(and DEBUG (or WINDOWS_PHONE XAMARIN))"   
 
-let lexbuf =  Lexing.LexBuffer<_>.FromString x
+let lexbuf = Lexing.LexBuffer<_>.FromString x
 while not lexbuf.IsPastEndOfStream do  
     printfn "%A" (TestLexer.tokenize lexbuf)
 
 Console.WriteLine("(press any key)")   
 Console.ReadKey(true) |> ignore
+*)
+
+open System
+open System.IO
+open Microsoft.FSharp.Text
+open Microsoft.FSharp.Text.Lexing
+open GraphLexer
+
+let test =
+    use stream_reader = new StreamReader ("/Users/Louis/Code/VietPhon/graph.dot")
+    let lexbuf = Lexing.LexBuffer<_>.FromTextReader (stream_reader)
+    while true do
+        try
+            printf "%A\n" (GraphLexer.tokenize lexbuf)
+        with
+            | ex -> Environment.Exit 1
+
