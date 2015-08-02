96b0e91587979a1b70c722ccc6582ad0ca1ec668
Louis Lecailliez <louis.lecailliez@outlook.fr>
2015-08-03 00:58:21 +0200

    Parser quasiment terminé.

--- FsmParser/Program.fs
+++ FsmParser/Program.fs
@@ -1,29 +1,4 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-(*
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
-*)
-
-(*
-open System
-open TestLexer
-open Microsoft.FSharp.Text
-open Microsoft.FSharp.Text.Lexing
-  
-let x = "(and DEBUG (or WINDOWS_PHONE XAMARIN))"   
-
-let lexbuf = Lexing.LexBuffer<_>.FromString x
-while not lexbuf.IsPastEndOfStream do  
-    printfn "%A" (TestLexer.tokenize lexbuf)
-
-Console.WriteLine("(press any key)")   
-Console.ReadKey(true) |> ignore
-*)
-
-open System
+﻿open System
 open System.IO
 open Microsoft.FSharp.Text
 open Microsoft.FSharp.Text.Lexing
@@ -45,28 +20,49 @@ let parse (path:string) =
     printf "Start Parsing...\n"
     GraphParser.start GraphLexer.tokenize lexbuf
 
+let print_result (table: (string * string * char) list) = 
+    List.map (fun (n1, n2, t) -> Console.WriteLine("transition :" + n1 + " " + n2 + " " + t.ToString())) table
 
- // temp à refactorer pour ne pas faire doublon avec la précédente
-let tokenize_from_string (content:string) =
-    printf "Start Lexing...\n"
-    let lexbuf = Lexing.LexBuffer<_>.FromString (content)
-    let mutable loop = true
-    while loop do
-        try
-            printf "%A\n" (GraphLexer.tokenize lexbuf)
-        with
-            | ex -> loop <- false ; ()
+let rec indent (str, depth) = if depth <= 0 then str else indent("\t" + str, depth - 1)
 
-let string_test = "strict digraph { //lol \n Finale0_2 -> Finale2[label=\"c,h\"]; Begin -> Finale0_2[label=\"h\"]; }"
-let parse_test x  = 
-    printf "Start Parsing...\n"
-    let lexbuf = Lexing.LexBuffer<_>.FromString x
-    GraphParser.start GraphLexer.tokenize lexbuf
+let create_fsm_file (table: (string * string * char) list) = 
+    let method_content = 
+        table 
+        |> Seq.groupBy (fun (a, _, _) -> a)
+        |> Seq.distinct
+        |> Seq.map (fun (key,value) -> String.Concat (indent(key, 2), ":\n",
+            indent(key, 3) + "FunctionHandler()\n",
+            indent("yield return null;\n", 3), 
+            String.Concat(Seq.map (fun (_, n, t) -> String.Format(indent("if(actionParameter == '{0}') goto {1};\n", 3), t, n)) value), 
+            indent("goto End;\n", 3)))
+        |> String.Concat
+
+    let partial_methods = 
+        table 
+        |> Seq.distinctBy (fun (n, _, _) -> n)
+        |> Seq.map (fun (n, _, _) -> n)
+        |> Seq.map (fun a -> indent(String.Format("partial void {0}FunctionHandler();\n", a), 2)) 
+        |> String.Concat
+
+    let file_content = 
+        String.Concat("namespace VietSyllableTransducer\n{\n", indent("partial class class VietSyllableTransducer\n\t{\n", 1),
+            indent("protected override IEnumerable WalkStates ()\n\t\t{\n", 2), 
+            method_content,
+            indent("End: ;", 2),
+            indent("\n}\n", 2),
+            partial_methods,
+            indent("\n}", 1),
+            indent("\n}", 0))
+
+    use output_file = new StreamWriter("output.cs", false, System.Text.Encoding.UTF8)
+    output_file.Write(file_content)
+    0
+
 
 [<EntryPoint>]
 let main argv = 
+    let pwet = tokenize "/Users/Louis/Code/VietPhon/graph.dot"
     let lol = parse "/Users/Louis/Code/VietPhon/graph.dot"
-    tokenize_from_string string_test
-    let result = parse_test string_test
+    //print_result (GraphParser.tr_table |> List.ofSeq)
+    create_fsm_file (GraphParser.tr_table |> List.ofSeq)
     0
-
